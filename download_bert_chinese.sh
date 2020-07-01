192609544cd61eb45d420cc6c88252df96ec4bb0
zcyzhuangzhou <2738524026@qq.com>
2020-07-01 15:11:19 +0800

    提交

---
+++ download_bert_chinese.sh
@@ -0,0 +1,4 @@
+mkdir bert-base-chinese
+wget http://52.216.242.246/models.huggingface.co/bert/bert-base-chinese-vocab.txt
+wget http://52.216.242.246/models.huggingface.co/bert/bert-base-chinese-config.json
+wget http://52.216.242.246/models.huggingface.co/bert/bert-base-chinese-pytorch_model.bin
